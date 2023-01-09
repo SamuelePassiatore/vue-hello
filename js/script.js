6fed0d997fa2b09b839f471f5c449b4c1f4f34e1
SamuelePassiatore <semmisno2@protonmail.com>
2023-01-09 15:34:02 +0100

    print message on page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
+const app = Vue.createApp({
+    data(){
+        return {
+            message: 'Hello Vue!!'
+        }
+    }
+});
+
+app.mount('#root');
